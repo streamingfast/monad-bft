4b3bcdca50feb9a9db73e834be7ee82c3e79bece
Ulysse Corbeil <ulysse98@hotmail.com>
2026-01-26 14:38:47 -0500

    test balance again

--- monad-eth-txpool/src/pool/mod.rs
+++ monad-eth-txpool/src/pool/mod.rs
@@ -192,9 +192,9 @@ where
             if account_balances
                 .get(tx.signer_ref())
                 .is_none_or(|account_balance_state| {
-                    let upfront_cost = tx.value()
-                        .saturating_add(tx.max_fee_per_gas().saturating_mul(tx.gas_limit()));
-                    account_balance_state.balance < upfront_cost
+                    tx.apply_max_value(account_balance_state.balance)
+                        .and_then(|balance| tx.apply_max_gas_cost(balance))
+                        .is_none()
                 })
             {
                 event_tracker.drop(tx.hash(), EthTxPoolDropReason::InsufficientBalance);
